9e14b3f23d5bc265a1f26e35512af08227c9832a
Polina <polinakoctyu4enko@gmail.com>
2024-10-23 23:06:27 +0300

    HW-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -34,17 +34,25 @@
 
 // export default App
 
-import userDate from "../Profile/userData.json";
+import userData from "../Profile/userData.json";
+import friends from "../FriendList/friends.json";
+import transactions from "../TransactionHistory/transactions.json";
 import Profile from "../Profile/Profile.jsx";
+import FriendList from "../FriendList/FriendList.jsx";
+import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx"
 
 export default function App() {
   return (
+    <>
     <Profile
-    username = {userDate.username}
-    tag = { userDate.tag }
-  location = { userDate.location }
-  avatar = { userDate.image }
-      stats={userDate.stats}
-      />
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </>
   )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,44 @@
+import PropTypes from "prop-types";
+import css from "./Profile.module.css"
 const Profile = ({
     username,
     tag,
     location,
-    image,
-    stats,
+    avatar,
+    stats: { followers, views, likes },
 }) => {
-    return
-    <div>
-        <div>
-            <img src={image} alt="User image" />
-            <p>{username}</p>
-            <p>{tag}</p>
-            <p>{location}</p>
+    return (
+        <div className={css.wrapper}>
+            <div className={css.card}>
+                <img src={avatar} alt="User avatar" className={css.avatar} />
+                <p className={css.userName}>{username}</p>
+                <p className={css.userinfo}>&#64;{tag}</p>
+                <p className={css.userinfo}>{location}</p>
+            </div>
+            <ul className={css.stats}>
+                <li className={css.statsItem}>
+                    <span className={css.statsItemLabel}>Followers</span>
+                    <span className={css.statsItemQuantity}>{followers}</span>
+                </li>
+                <li className={css.statsItem}>
+                    <span className={css.statsItemLabel}>Viewes</span>
+                    <span className={css.statsItemQuantity}>{views}</span>
+                </li>
+                <li className={css.statsItem}>
+                    <span className={css.statsItemLabel}>Likes</span>
+                    <span className={css.statsItemQuantity}>{likes}</span>
+                </li>
+            </ul>
         </div>
-    </div>
+    )
 }
+
+Profile.propTypes = {
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
+    stats: PropTypes.objectOf(PropTypes.number),
+  };
+
+export default Profile;
